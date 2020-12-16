dff12c46107131761065f0901d3053c46e09f308
eshaan13 <49763476+eshaan13@users.noreply.github.com>
2020-12-16 15:50:39 +0000

    Add files via upload

---
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -0,0 +1,66 @@
+package uk.ac.ed.inf.aqmaps;
+
+public class Sensor {
+	
+	private String location;
+	private double battery;
+	private String reading;
+	private Position pos;
+	private int sensorNumber;
+	private String rgbValue;
+	private String markerSymbol;
+	private boolean visited;
+	
+	
+	public String getLocation() {
+		return location;
+	}
+	public double getBattery() {
+		return battery;
+	}
+	public String getReading() {
+		return reading;
+	}
+	public Position getPos() {
+		return pos;
+	}
+	public void setPos(Position pos) {
+		this.pos = pos;
+	}
+	public int getSensorNumber() {
+		return sensorNumber; 
+	}
+	public void setSensorNumber(int sensorNumber) {
+		this.sensorNumber = sensorNumber; 
+	}
+	public String getRgbValue() {
+		return rgbValue;
+	}
+	public void setRgbValue(String rgbValue) {
+		this.rgbValue = rgbValue;
+	}
+	public String getMarkerSymbol() {
+		return markerSymbol;
+	}
+	public void setMarkerSymbol(String markerSymbol) {
+		this.markerSymbol = markerSymbol;
+	}
+	public boolean isVisited() {
+		return visited;
+	}
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+	/**
+	 * Method to check if the drone is in range of the sensor or not
+	 * @param p position of the drone
+	 * @param sensor sensor to be read
+	 * @return true if the drone is in range of the sensor, otherwise false
+	 */
+	boolean inRange(Position position) {
+		if(position.distance(pos) < 0.0002) // finding the distance between the sensor and the current position 
+												// of the drone and checking if its less than 0.0002 degrees
+			return true; // drone is in range
+		else return false;  // drone is not in range
+	}
+}
